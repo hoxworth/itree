ec53ad2648e84e7518ddc997d6c66e19d9524d49
Kenny <hoxworth@gmail.com>
2013-07-16 15:44:35 -0700

    Added tree insert method, building out specs for insert

--- lib/itree/tree.rb
+++ lib/itree/tree.rb
@@ -6,5 +6,113 @@ module Intervals
 			@root = nil
 			@size = 0
 		end
+
+		def insert(leftScore, rightScore, data)
+			node = Node.new(leftScore,rightScore,data)
+			success = true
+
+			if @root.nil?
+				@root = node
+			else
+				balance,success = insertNode(@root,node)
+			end
+
+			@size = @size + 1 if success
+			return success
+		end
+
+		private
+		def insertNode(locNode,insertNode,updateData=false)
+			diff = locNode <=> insertNode
+
+			if diff > 0
+				if locNode.left.nil?
+					locNode.left = insertNode
+					insertNode.parent = locNode
+					locNode.balance = locNode.balance - 1
+					locNode.updateMaxScores
+					if locNode.balance == 0
+						return 0,true
+					else
+						return 1,true
+					end
+				else
+					balanceUpdate,success = insertNode(locNode.left,insertNode,updateData)
+					if balanceUpdate != 0
+						locNode.balance = locNode.balance - 1
+						if locNode.balance == 0
+							return 0,success
+						elsif locNode.balance == -1
+							return 1,success
+						end
+
+						if locNode.left.balance < 0
+							rightRotation(locNode)
+							locNode.balance = 0
+							locNode.parent.balance = 0
+							locNode.subLeftMax = locNode.parent.subRightMax
+							locNode.parent.subRightMax = -Float::INFINITY
+						else
+							leftRotation(locNode.left)
+							rightRotation(locNode)
+							locNode.parent.resetBalance
+
+							locNode.subLeftMax = locNode.parent.subRightMax
+							locNode.parent.left.subRightMax = locNode.parent.subLeftMax
+							locNode.parent.subRightMax = -Float::INFINITY
+							locNode.parent.subLeftMax = -Float::INFINITY
+						end
+
+						locNode.parent.updateMaxScores
+					end
+					return 0,success
+				end
+			elsif diff < 0
+				if locNode.right.nil?
+					locNode.right = insertNode
+					insertNode.parent = locNode
+					locNode.balance = locNode.balance + 1
+					locNode.updateMaxScores
+					if locNode.balance == 0
+						return 0,true
+					else
+						return 1,true
+					end
+				else
+					balanceUpdate,success = insertNode(locNode.right,insertNode,updateData)
+					if balanceUpdate != 0
+						locNode.balance = locNode.balance + 1
+						if locNode.balance == 0
+							return 0,success
+						elsif locNode.balance == 1
+							return 1,success
+						end
+
+						if locNode.right.balance > 0
+							leftRotation(locNode)
+							locNode.balance = 0
+							locNode.parent.balance = 0
+							locNode.subRightMax = locNode.parent.subLeftMax
+							locNode.parent.subLeftMax = -Float::INFINITY
+						else
+							rightRotation(locNode.right)
+							leftRotation(locNode)
+							locNode.parent.resetBalance
+
+							locNode.subRightMax = locNode.parent.subLeftMax
+							locNode.parent.right.subLeftMax = locNode.parent.subRightMax
+							locNode.parent.subRightMax = -Float::INFINITY
+							locNode.parent.subLeftMax = -Float::INFINITY
+						end
+
+						locNode.parent.updateMaxScores
+					end
+					return 0,success
+				end
+			else
+				locNode.data = insertNode.data if updateData
+				return 0,false
+			end
+		end
 	end
 end
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,10 +1,39 @@
 require 'spec_helper'
 
 describe Intervals::Tree do
+	let(:tree) { Intervals::Tree.new }
+
 	context "creation" do
 		describe "#initialize" do
 			it { Intervals::Tree.new.size.should eq 0}
 			it { Intervals::Tree.new.root.should be nil}
 		end
+	end
+
+	describe "#insert" do
+		context "tree is empty" do
+			it "inserts a new node at root" do
+				tree.insert(0,10,nil)
+				tree.root.scores.should eq [0,10]
+			end
+		end
+
+		context "inserting duplicates" do
+			before(:each) { tree.insert(0,10,nil)}
+
+			it { tree.insert(0,10,"taco").should be false}
+			it "should not increase the size" do
+				tree.insert(0,10,"taco")
+				tree.size.should_not eq 2
+			end
+		end
+	end
+
+	describe "#remove" do
+
+	end
+
+	describe "#stab" do
+
 	end
 end
