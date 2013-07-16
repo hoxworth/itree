fafe5ca10383b8b494237072d11ca4b5e3e5a1f1
Kenny <hoxworth@gmail.com>
2013-07-15 22:48:16 -0700

    Throw an ArgumentError when min > max

--- lib/itree/node.rb
+++ lib/itree/node.rb
@@ -3,6 +3,7 @@ module Intervals
 		attr_accessor :data, :scores, :subLeftMax, :subRightMax, :balance, :left, :right, :parent
 
 		def initialize(min,max,data)
+			raise ArgumentError.new("first agument cannot be greater than second argument") if min > max
 			@scores = [min,max]
 			@subLeftMax = nil
 			@subRightMax = nil
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -6,6 +6,7 @@ describe Intervals::Node do
 			it { Intervals::Node.new(1,2,"taco").scores.should eq [1,2]}
 			it { Intervals::Node.new(1,2,"taco").data.should eq "taco"}
 			it { Intervals::Node.new(1,2,"taco").balance.should eq 0}
+			it { expect { Intervals::Node.new(2,1,"taco") }.to raise_error(ArgumentError)}
 		end
 	end
 
