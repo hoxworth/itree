8b51bd3ad76ff1f664be9ea60122608048736069
Kenny <hoxworth@gmail.com>
2013-07-15 22:44:44 -0700

    Adding initial rspec

---
+++ spec/node_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Intervals::Node do
+	context "creation" do
+		describe "#initialize" do
+			it { Intervals::Node.new(1,2,"taco").scores.should eq [1,2]}
+			it { Intervals::Node.new(1,2,"taco").data.should eq "taco"}
+			it { Intervals::Node.new(1,2,"taco").balance.should eq 0}
+		end
+	end
+
+	context "comparing" do
+		describe "#<=>" do
+		end
+
+		describe "#==" do
+		end
+	end
+
+	describe "#insert" do
+	end
+
+	describe "#resetBalance" do
+	end
+
+	describe "#updateMaxScores" do
+
+	end
+
+end
