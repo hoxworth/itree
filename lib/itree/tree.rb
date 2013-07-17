9255e3390f5b1c53c96c8a55e2e991ab522a9689
Kenny <hoxworth@gmail.com>
2013-07-16 21:56:50 -0700

    Remove code

--- lib/itree/tree.rb
+++ lib/itree/tree.rb
@@ -32,6 +32,19 @@ module Intervals
 			results
 		end
 
+		def remove(leftScore, rightScore)
+			if @root
+				delNode = Node.new(leftScore,rightScore,nil)
+				height,removed = removeNode(@root,delNode)
+				if removed
+					@size = @size - 1
+					@root = nil if @size == 0
+					return true
+				end
+			end
+			return false
+		end
+
 		private
 		def insertNode(locNode,insertNode,updateData=false)
 			diff = locNode <=> insertNode
@@ -124,6 +137,188 @@ module Intervals
 				locNode.data = insertNode.data if updateData
 				return 0,false
 			end
+		end
+
+		def removeFromParent(locNode,replacementNode)
+			if locNode.parent
+				if locNode.parent.left == locNode
+					locNode.parent.left = replacementNode
+				else
+					locNode.parent.right = replacementNode
+				end
+			else
+				@root = replacementNode
+			end
+		end
+
+		def removeNode(locNode, delNode)
+			diff = locNode <=> delNode
+			heightDelta = 0
+			replacementNode = nil
+			removed = false
+
+			if diff == 0
+				if locNode.left.nil?
+					if locNode.right.nil?
+						removeFromParent(locNode,nil)
+						locNode.parent.updateMaxScores if locNode.parent
+						removed = true
+						return -1,removed
+					end
+					removeFromParent(locNode,locNode.right)
+					locNode.right.parent = locNode.parent
+					locNode.parent.updateMaxScores if locNode.parent
+					locNode.right = nil
+					removed = true
+					return -1,removed
+				end
+
+				if locNode.right.nil?
+					removeFromParent(locNode,locNode.left)
+					locNode.left.parent = locNode.parent
+					locNode.parent.updateMaxScores if locNode.parent
+					locNode.left = nil
+					removed = true
+					return -1,removed
+				end
+
+				if locNode.balance < 0
+					replacementNode = locNode.left
+					while replacementNode.right do
+						replacementNode = replacementNode.right
+					end
+				else
+					replacementNode = locNode.right
+					while replacementNode.left do
+						replacementNode = replacementNode.left
+					end
+				end
+
+				heightDelta,removed = removeNode(locNode,replacementNode)
+
+				locNode.right.parent = replacementNode if locNode.right
+				locNode.left.parent = replacementNode if locNode.left
+
+				replacementNode.left = locNode.left
+				replacementNode.right = locNode.right
+				replacementNode.parent = locNode.parent
+				replacementNode.balance = locNode.balance
+
+				if locNode == @root
+					@root = replacementNode
+					replacementNode.updateMaxScores
+				else
+					if locNode == locNode.parent.left
+						locNode.parent.left = replacementNode
+					else
+						locNode.parent.right = replacementNode
+					end
+					replacementNode.updateMaxScores
+				end
+
+				locNode.left = nil
+				locNode.right = nil
+
+				if replacementNode.balance == 0
+					return heightDelta,removed
+				end
+				removed = true
+				return 0,removed
+			elsif diff > 0
+				if locNode.left
+					heightDelta,removed = removeNode(locNode.left,delNode)
+					if heightDelta
+						locNode.balance = locNode.balance + 1
+						if locNode.balance == 0
+							return -1,removed
+						elsif locNode.balance == 1
+							return 0,removed
+						end
+
+						if locNode.right.balance == 1
+							leftRotation(locNode)
+							locNode.parent.balance = 0
+							locNode.parent.left.balance = 0
+
+							locNode.subRightMax = locNode.parent.subLeftMax
+							locNode.parent.subLeftMax = nil
+							locNode.parent.updateMaxScores
+
+							return -1,removed
+						elsif locNode.right.balance == 0
+							leftRotation(locNode)
+							locNode.parent.balance = -1
+							locNode.parent.left.balance = 1
+
+							locNode.subRightMax = locNode.parent.subLeftMax
+							locNode.parent.subLeftMax = nil
+							locNode.parent.updateMaxScores
+
+							return 0,removed
+						end
+						rightRotation(locNode.right)
+						leftRotation(locNode)
+						locNode.parent.resetBalance
+
+						locNode.subRightMax = locNode.parent.subLeftMax
+						locNode.parent.right.subLeftMax = locNode.parent.subRightMax
+
+						locNode.parent.subRightMax = nil
+						locNode.parent.subLeftMax = nil
+						locNode.parent.updateMaxScores
+
+						return -1,removed
+					end
+				end
+			elsif diff < 0
+				if locNode.right
+					heightDelta,removed = removeNode(locNode.right,delNode)
+					if heightDelta
+						locNode.balance = locNode.balance - 1
+						if locNode.balance == 0
+							return 1,removed
+						elsif locNode.balance == -1
+							return 0,removed
+						end
+
+						if locNode.left.balance == -1
+							rightRotation(locNode)
+							locNode.parent.balance = 0
+							locNode.parent.right.balance = 0
+
+							locNode.subLeftMax = locNode.parent.subRightMax
+							locNode.parent.subRightMax = nil
+							locNode.parent.updateMaxScores
+
+							return -1,removed
+						elsif locNode.left.balance == 0
+							rightRotation(locNode)
+							locNode.parent.balance = 1
+							locNode.parent.right.balance = -1
+
+							locNode.subLeftMax = locNode.parent.subRightMax
+							locNode.parent.subRightMax = nil
+							locNode.parent.updateMaxScores
+
+							return 0,removed
+						end
+						leftRotation(locNode.left)
+						rightRotation(locNode)
+						locNode.parent.resetBalance
+
+						locNode.subLeftMax = locNode.parent.subRightMax
+						locNode.parent.left.subRightMax = locNode.parent.subLeftMax
+
+						locNode.parent.subLeftMax = nil
+						locNode.parent.subRightMax = nil
+						locNode.parent.updateMaxScores
+
+						return -1,removed
+					end
+				end
+			end
+
+			return 0,removed
 		end
 
 		def leftRotation(locNode)
